ad517cf783d8532d645a4e538dac2b173aced6ed
Mauricio Galvis <fmhernandezg@ufpso.edu.co>
2025-08-27 22:37:08 -0500

    semana ll

---
+++ Semana2/app1/src/Semana/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+    public class Main {
+        
+            public static void main(String[] args) throws Exception {
+            Scanner scanner = new Scanner(System.in);
+
+            Product product = new Product();
+            product.register(scanner);
+            product.sell(scanner);
+            product.newstock(scanner);
+
+        }
+
+}
---
+++ Semana2/app1/src/Semana/Product.java
@@ -0,0 +1,46 @@
+package app1;
+
+
+import java.util.Scanner;
+
+public class Product {
+    String name;
+    double price;
+    int quantity;
+    int amount;
+
+    void register(Scanner scanner) {
+        System.out.print("Enter product name: ");
+        this.name = scanner.nextLine();
+        System.out.print("Enter product price: ");
+        this.price = scanner.nextDouble();
+        System.out.print("Enter product quantity: ");
+        this.quantity = scanner.nextInt();
+
+        System.out.println("Producto registrado:");
+        System.out.println("Nombre: " + this.name);
+        System.out.println("Precio: " + this.price);
+        System.out.println("Cantidad: " + this.quantity);
+    }
+
+    void sell(Scanner scanner) {
+        System.out.print("Enter product quantity to sell: ");
+        this.amount = scanner.nextInt();
+        if (this.quantity > this.amount) {
+            this.quantity -= this.amount;
+            System.out.println("vendido:" + amount + " | nuevo stock: " + this.quantity);
+        } else {
+            System.out.println("cantidad insuficiente, solo hay: " + this.quantity + " en stock.");
+        }
+    }
+
+    void newstock(Scanner scanner){
+        System.out.print("Enter new stock quantity: ");
+        this.amount = scanner.nextInt();
+
+        this.quantity += this.amount;
+        System.out.println("nuevo stock: " + this.quantity);
+
+    }
+
+}
